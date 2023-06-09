4f5a6c5e4297cd0252a2a4cafe25e47ec9d07183
christianjjc <christianjjc@gmail.com>
2023-06-09 14:34:54 -0500

    login terminado - parte 1

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,81 +1,77 @@
-import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import UtilidadesCj from "../../utils/utilitarios.js";
 
 const Login = () => {
-  const navigate = useNavigate();
-  const [usuario, setUsuario] = useState([]);
-  let mensajeError = "";
+    const navigate = useNavigate();
+    const [usuario, setUsuario] = useState([]);
 
-  const handleLogin = async () => {
-    let data = JSON.stringify({
-      nombre_usuario: document.getElementById("txtUserName").value,
-      pass_usuario: document.getElementById("txtPassword").value,
-    });
-    let config = {
-      method: "post",
-      maxBodyLength: Infinity,
-      url: "http://localhost:8080/",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      data: data,
+    const handleLogin = async () => {
+        try {
+            let data = JSON.stringify({
+                nombre_usuario: document.getElementById("txtUserName").value,
+                pass_usuario: document.getElementById("txtPassword").value,
+            });
+            const result = await UtilidadesCj.obtenerDatos("http://localhost:8080/", "post", data);
+            if (result.length > 0) {
+                setUsuario(result);
+            } else {
+                document.getElementById("txtMensajeError").classList.remove("d-none");
+                ocultaMensaje();
+            }
+        } catch (error) {
+            console.log(error);
+        }
     };
-    await axios
-      .request(config)
-      .then((response) => {
-        setUsuario(response.data);
-        //setUsuario(response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
 
-  const obtenerDatos = () => {};
+    const ocultaMensaje = () => {
+        setTimeout(() => {
+            document.getElementById("txtMensajeError").classList.add("d-none");
+        }, 5 * 1000);
+    };
 
-  useEffect(() => {
-    if (usuario.length > 0) {
-      navigate("/main-menu");
-    } else {
-      console.log("no encontrado");
-    }
-  }, [navigate, usuario]);
+    useEffect(() => {
+        if (usuario.length > 0) {
+            navigate("/main-menu");
+        }
+    }, [navigate, usuario]);
 
-  return (
-    <>
-      <div>
-        <div className="row justify-content-center">
-          <div className="col-md-6">
-            <h3 className="text-center mb-4">Ingreso al Sistema</h3>
-            <div className="mb-3">
-              <label htmlFor="txtUserName" className="form-label">
-                Nombre de usuario:
-              </label>
-              <input type="text" className="form-control" id="txtUserName" placeholder="Ingrese su nombre de usuario" />
+    return (
+        <>
+            <div>
+                <div className="row justify-content-center">
+                    <div className="col-md-6">
+                        <h3 className="text-center mb-4">Ingreso al Sistema</h3>
+                        <div className="mb-3">
+                            <label htmlFor="txtUserName" className="form-label">
+                                Nombre de usuario:
+                            </label>
+                            <input type="text" className="form-control" id="txtUserName" placeholder="Ingrese su nombre de usuario" />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="txtPassword" className="form-label">
+                                Contraseña:
+                            </label>
+                            <input type="password" className="form-control" id="txtPassword" placeholder="Ingrese su contraseña" />
+                        </div>
+                        <div className="text-center mb-3">
+                            <a href="#" className="text-decoration-none">
+                                ¿Olvidaste la contraseña?
+                            </a>
+                        </div>
+                        <div className="text-center">
+                            <button type="button" className="btn btn-primary" onClick={handleLogin}>
+                                Ingresar
+                            </button>
+                        </div>
+                        <div className="alert alert-warning d-none my-2" role="alert" id="txtMensajeError">
+                            Los datos ingresados no son correctos
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="mb-3">
-              <label htmlFor="txtPassword" className="form-label">
-                Contraseña:
-              </label>
-              <input type="password" className="form-control" id="txtPassword" placeholder="Ingrese su contraseña" />
-            </div>
-            <div className="text-center mb-3">
-              <a href="#" className="text-decoration-none">
-                ¿Olvidaste la contraseña?
-              </a>
-            </div>
-            <div className="text-center">
-              <button type="button" className="btn btn-primary" onClick={handleLogin}>
-                Ingresar
-              </button>
-            </div>
-            <div className="text-center">{mensajeError}</div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+        </>
+    );
 };
 
 export default Login;
--- src/utils/utilitarios.js
+++ src/utils/utilitarios.js
@@ -1,5 +1,26 @@
-import moment from "moment";
+//import moment from "moment";
+import axios from "axios";
 
-class UtilidadesCj {}
+class UtilidadesCj {
+  static obtenerDatos = async (ruta, vervo, array) => {
+    let config = {
+      method: vervo,
+      maxBodyLength: Infinity,
+      url: ruta,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: array,
+    };
+    try {
+      const response = await axios.request(config);
+      console.log(response.data);
+      return response.data;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  };
+}
 
 export default UtilidadesCj;
